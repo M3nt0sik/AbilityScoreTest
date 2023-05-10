70c063c0d91347eed767fb5062984be90558b5f6
Kamil <mentos.96@o2.pl>
2023-05-10 22:39:42 +0200

    write 2 funcion ReadInt and ReadDouble

--- Program.cs
+++ Program.cs
@@ -15,5 +15,46 @@ while (true)
     char keyChar = Console.ReadKey(true).KeyChar;
     if (keyChar == 'Q' || keyChar == 'q') return;
 
+    ReadInt
+}
+///<summary>
+///Wyswietla informacje  i wczytuje wartosci typu int z konsoli
+/// </summary>
+/// <param name="lastUsedValue"> Wartosc omyslna </param>
+/// <param name="prompt"> Informacja wyswietlanaw konsoli</param>
+/// <returns>Wczytana wartosc int lub domyslna 
+/// (jezeli nie można przetworzyc wczytanej wartosci) </returns>
+static int ReadInt(int lastUsedValue, string prompt)
+{
+    Console.Write(prompt + "["+ lastUsedValue+"]: ");
+    bool toIf = int.TryParse(Console.ReadLine(), out int read);
+    if (toIf)
+    {
+        Console.WriteLine("Urzycie wartosci " + read);
+        return read;
+        }
+    else
+    {
+        Console.WriteLine("Urzycie wartosci domyslnej " + lastUsedValue);
+        return lastUsedValue;
+    }
+    
+
+}
+static double ReadDouble(double lastUsedValue, string prompt)
+{
+    Console.Write(prompt + "[" + lastUsedValue + "]: ");
+    bool toIf = double.TryParse(Console.ReadLine(), out double read);
+    if (toIf)
+    {
+        Console.WriteLine("Urzycie wartosci " + read);
+        return read;
+    }
+    else
+    {
+        Console.WriteLine("Urzycie wartosci domyslnej " + lastUsedValue);
+        return lastUsedValue;
+    }
+
 
 }
