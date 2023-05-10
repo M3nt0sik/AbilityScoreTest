08bd23feb819dcd810012673f1a465c123c82a55
Kamil <mentos.96@o2.pl>
2023-05-10 21:47:11 +0200

    Write skieleton function main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AbilityScoreTest;
+
+
+Class_AbilityScoreCalculator calculator = new Class_AbilityScoreCalculator();
+while (true)
+{
+    calculator.RollResult = ReadInt(calculator.RollResult, "Poczatkowy rzut 4d6");
+    calculator.DividedBy = ReadDouble(calculator.DividedBy, "Dzielone przez");
+    calculator.AddAmount = ReadInt(calculator.AddAmount, "Dodawana wartosc");
+    calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+    calculator.CalculateAbilityScore();
+    Console.WriteLine("Obliczone punkty umiejetnosci: " + calculator.Score);
+    Console.WriteLine("Wcisnij Q by zakonczyc lub inny klawisz by kontynuowac");
+    char keyChar = Console.ReadKey(true).KeyChar;
+    if (keyChar == 'Q' || keyChar == 'q') return;
+
+
+}
